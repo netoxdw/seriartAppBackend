d288b513029d9313648d63e7aab96533853de857
netoxdw <neto.guevara.tapia@gmail.com>
2026-04-16 17:44:31 -0600

    bug de form en PedidoItemAnilloForm solucionado. sigiente paso, form dinamico de pedidoitembase

--- apps/ventas/form.py
+++ apps/ventas/form.py
@@ -1,5 +1,5 @@
 from django import forms
-from apps.catalogo.models import Modelo
+from apps.catalogo.models import Modelo, Anillo
 from .models import PedidoItemBase, PedidoItemAnillo, PedidoItemExtra, Observacion, Pago
 
 
