0df5319c7aa213269b8280ef2f1e5a0221ac5311
netoxdw <neto.guevara.tapia@gmail.com>
2026-06-02 14:58:05 -0600

    settings.py para entorno de desarrollo

--- config/settings.py
+++ config/settings.py
@@ -6,7 +6,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # Seguridad
 SECRET_KEY = config('SECRET_KEY')
-DEBUG = config('DEBUG', default=False, cast=bool)
+DEBUG = True
 
 ALLOWED_HOSTS = []
 
