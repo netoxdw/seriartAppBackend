36b72224a6974516d4d71bad3d50aeb1e1ae9fd1
netoxdw <neto.guevara.tapia@gmail.com>
2026-06-04 22:18:36 -0600

    import export en settings.py

--- config/settings.py
+++ config/settings.py
@@ -19,6 +19,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
+    'import_export',  # ← agrega esto
+
     'apps.escuelas',
     'apps.alumnos',
     'apps.catalogo',
